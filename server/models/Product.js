a8d36d4cceeb2cf2d467649815b78db60f4f6708
SkyPKWolf <pashokkuzmenko@gmail.com>
2023-02-16 15:35:37 +0200

    backend init

---
+++ server/models/Product.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+
+const reviewShema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    title: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  },
+  { timestamps: true }
+);
+
+const productShema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: { reviewShema },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numberOfReviews: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    productIsNew: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model('Product', productShema)
+
+export default Product
