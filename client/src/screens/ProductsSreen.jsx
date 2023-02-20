669527ebff0617d46161e54a6d2771bd5613a1d2
SkyPKWolf <pashokkuzmenko@gmail.com>
2023-02-20 12:43:15 +0200

    redux setup and products fetched

--- client/src/screens/ProductsSreen.jsx
+++ client/src/screens/ProductsSreen.jsx
@@ -1,18 +1,52 @@
-import {Center, Wrap, WrapItem} from '@chakra-ui/react'
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Center,
+  Spinner,
+  Stack,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
 import ProductCart from '../components/ProductCart';
-import {products} from '../products';
-const ProductsSreen = () => {
-  return (
-    <Wrap spacing='30px' justify='center' minHeight='100vh' >
-        {products.map((product) => (
-            <WrapItem key={product._id} >
-                <Center w='250px' h='550px' >
-                    <ProductCart product={product} />
-                </Center>
-            </WrapItem>
-        ))}
-    </Wrap>
-  )
-}
+import { products } from '../products';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts } from '../redux/actions/productActions';
+import { useEffect } from 'react';
 
-export default ProductsSreen
+const ProductsSreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.products);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <Wrap spacing='30px' justify='center' minHeight='100vh'>
+      {loading ? (
+        <Stack spacing={4} direction='row'>
+          <Spinner mt={20} thickness='2px' speed='0.65s' emptyColor='gray.200' color='orange.500' size='xl' />
+        </Stack>
+      ) : error ? (
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle>Upps!!!</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      ) : (
+        products.map((product) => (
+          <WrapItem key={product._id}>
+            <Center w='250px' h='550px'>
+              <ProductCart product={product} />
+            </Center>
+          </WrapItem>
+        ))
+      )}
+    </Wrap>
+  );
+};
+
+export default ProductsSreen;
